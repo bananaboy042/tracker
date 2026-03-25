c86f344c2c0f65a4bfaeb6bf7c2f3ff076b04665
Vanik <keviapelsinik@gmail.com>
2026-03-25 13:31:48 +0300

    dobavil vozvrat dannih pri oshibke

--- tracker/users/views.py
+++ tracker/users/views.py
@@ -7,16 +7,28 @@ def register(request):
         return render(request, "register.html")
     else:
         login = request.POST.get('username')
+        user_number = request.POST.get('phone_number')
         if has_russian_letters(login):
-            return render(request, "register.html", context={'login_error': 'Логин не должен содержать русских букв'})
-        number = request.POST.get('phone_number')
-        number = validate_and_format_phone(number)
+            return render(request, "register.html", context={
+                'login_error': 'Логин не должен содержать русских букв',
+                'login': login,
+                'number': user_number
+                                                             })
+
+        number = validate_and_format_phone(user_number)
         if not number:
-            return render(request, "register.html", context={'phone_error': 'Неверный формат номера телефона'})
-        # Иван сделай пожалуйста проверку паролей
+            return render(request, "register.html", context={
+                'phone_error': 'Неверный формат номера телефона',
+                'login': login,
+                'number':user_number
+                                                             })
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
         if password1 != password2:
-            return render(request, "register.html", context={'password_error': 'Пароли не совпадают'})
+            return render(request, "register.html", context={
+                'password_error': 'Пароли не совпадают',
+                'login':login,
+                'number': user_number
+                                } )
         print(login,number,password1,password2)
         return render(request, "register.html")
