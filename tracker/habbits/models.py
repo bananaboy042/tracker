8f34d6c463c6dca53127310c3fc7cf1d78ea8fd6
Ваше Имя <you@example.com>
2026-06-19 13:25:54 +0300

    created habbit and color model

--- tracker/habbits/models.py
+++ tracker/habbits/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.db.models import Model, CharField, TextField, ForeignKey, PROTECT, DateField
+from django.utils.timezone import now
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Color(Model):
+    name = CharField(max_length=50, verbose_name="Название цвета")
+    hex_code = CharField(max_length=7, unique=True, verbose_name="HEX код цвета")
+
+
+class Habbit(Model):
+    name = CharField(max_length=100, verbose_name="Название привычки")
+    description = TextField(max_length=500, blank=True, verbose_name="Описание привычки")
+    color = ForeignKey(Color, on_delete=PROTECT, verbose_name="Цвет привычки")
+    start_date = DateField(default=now, verbose_name="Дата начала")
+    end_date = DateField(null=True, blank=True, verbose_name="Дата окончания")
+
+    def validate_dates(self):
+        """Метод валидации дат"""
+        today = now().date()
+
+        if self.start_date < today:
+            raise ValidationError({
+                'start_date': f'Дата начала не может быть раньше сегодняшнего дня'
+            })
+
+        if self.end_date:
+            if self.end_date < self.start_date:
+                raise ValidationError({
+                    'end_date': 'Дата окончания не может быть раньше даты начала'
+                })
+            if self.end_date < today:
+                raise ValidationError({
+                    'end_date': 'Дата окончания не может быть раньше сегодняшнего дня'
+                })
+
+    def save(self, *args, **kwargs):
+        """Переопределяем save для валидации"""
+        # Валидация перед сохранением
+        self.validate_dates()
+        super().save(*args, **kwargs)
+
+
+
