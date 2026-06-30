393824e98201e1a131ba283c9df3566b81ec8d2f
Vanik <keviapelsinik@gmail.com>
2026-06-30 14:25:12 +0300

    created a habbit template

--- tracker/habbits/views.py
+++ tracker/habbits/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from .models import Color
+
+
 # Create your views here.
 def tracker(request):
     return render(request, 'tracker.html')
 
 def habbit_add(request):
-    return render(request,'habbitadd.html')
+    if request.method == "GET":
+        colors = Color.objects.all()
+        return render(request,'habbitadd.html', context={'colors':colors})
+    else:
+        return render(request, 'habbitadd.html')
