70f4bf1cfdc002d670c07923e6f9e1b656de1036
Ваше Имя <you@example.com>
2026-07-09 15:55:24 +0300

    added validation by created habbit

--- tracker/habbits/views.py
+++ tracker/habbits/views.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 
 from .models import Color, Habbit
@@ -21,20 +23,32 @@ def habbit_add(request):
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
         frequency = request.POST.get('frequency_type')
-        if frequency == 'daily':
-            daily_interval = request.POST.get('daily_interval', '')
-            daily = {'type': frequency, 'interval': int(daily_interval)}
-            # print(daily_interval)
-        elif frequency == 'weekly':
-            week_days = request.POST.getlist('week_days', '')
-            daily = {'type': frequency, 'days': [int(item) for item in week_days]}
 
-            # print(week_days)
+        daily = {'type': frequency}
+        if frequency == 'daily':
+            daily_interval = request.POST.get('daily_interval', 1)
+            daily['interval'] = int(daily_interval)
+        elif frequency == 'weekly':
+            week_days = request.POST.getlist('week_days', [1])
+            daily['days'] = [int(item) for item in week_days]
         else:
-            monthly_day = request.POST.get('monthly_day', '')
-            daily = {'type': frequency, 'day': int(monthly_day)}
-#             print(monthly_day)
+            monthly_day = request.POST.get('monthly_day', 1)
+            daily['day'] = int(monthly_day)
         print(habbit_name, description, color_id, start_date, end_date, frequency,request.user )
-        Habbit.objects.create(name=habbit_name, description=description, color_id=color_id,
-                              start_date=start_date, end_date=end_date, frequency=daily, user=request.user)
-        return render(request, 'habbitadd.html',context={'colors':colors})
+
+        context = {'colors': colors}
+
+        try:
+            start_date = datetime.strptime(start_date, '%Y-%m-%d').date() if start_date else None
+            end_date = datetime.strptime(end_date, '%Y-%m-%d').date() if end_date else None
+        except Exception:
+            context['error_message'] = 'Неверный формат даты. Используйте формат ГГГГ-ММ-ДД'
+            return render(request, 'habbitadd.html', context=context)
+
+        try:
+            Habbit.objects.create(name=habbit_name, description=description, color_id=color_id,
+                                  start_date=start_date, end_date=end_date, frequency=daily, user=request.user)
+            return render(request, 'habbitadd.html', context=context)
+        except ValidationError as e:
+            context['error_message'] = e.messages[0]
+            return render(request, 'habbitadd.html', context=context)
